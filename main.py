c6f7cc8912cc040935884e3c460c0b77283e5229
SachinthaFernando99 <sachintharavindu46@gmail.com>
2024-11-14 00:41:54 +0530

    CORS Edit Commit

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
 from routers.role import router as role_router
 from routers.user import router as user_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(user_router, prefix="/users", tags=["Users"])
 app.include_router(role_router, prefix="/roles", tags=["Roles"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
