5b26ef35809671ce28542b948e3e80aa55449d74
SachinthaFernando99 <sachintharavindu46@gmail.com>
2024-11-17 00:15:47 +0530

    user changes commit

--- routers/user.py
+++ routers/user.py
@@ -16,61 +16,49 @@ def get_db():
 @router.get("/user")
 def read_user(db: Session = Depends(get_db)):
     user = db.query(User.first_name, User.last_name, UserRole.role_name).join(UserRole, User.role_id == UserRole.type_id).all()
-    user_list = [{"firstname": u.firstname, "lastname": u.lastname, "usertype": u.usertype} for u in user]
+    user_list = [{"firstname": u.first_name, "lastname": u.last_name, "rolename": u.role_name} for u in user]
     return user_list
 
-@router.get("/user_details/{id}")
-def get_user(user_id: int, db: Session = Depends(get_db)):
-    user = db.query(User.first_name, User.last_name, UserRole.role_name).join(UserRole, User.role_id == UserRole.type_id).filter(User.user_id == user_id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    return {
-        "first_name": user.firstname,
-        "last_name": user.lastname,
-        "user_type": user.usertype
-    }
+
 
 @router.post("/create-user")
-def create_user(user_id: int, firstname: str, lastname: str, roleid: int, db: Session = Depends(get_db)):
-    existing_user = db.query(User).filter(User.user_id == user_id).first()
-    if existing_user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="User ID already exists")
-    user_role = db.query(UserRole).filter(UserRole.type_id == roleid).first()
+def create_user(first_name: str, last_name: str, role_id: int, db: Session = Depends(get_db)):
+    user_role = db.query(UserRole).filter(UserRole.type_id == role_id).first()
     if not user_role:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid roleid: No such role exists")
-    db_user = User(user_id=user_id, firstname=firstname, lastname=lastname, roleid=roleid)
+    db_user = User(first_name=first_name, last_name=last_name, role_id=role_id)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return {
-        "user_id": db_user.user_id,
-        "firs_tname": db_user.first_name,
-        "last_name": db_user.last_name,
-        "role_id": db_user.role_id
+        "userid": db_user.user_id,
+        "firstname": db_user.first_name,
+        "lastname": db_user.last_name,
+        "roleid": db_user.role_id
     }
 
 @router.put("/update-user/{user_id}")
-def update_user(user_id: int, firstname: Optional[str] = None, lastname: Optional[str] = None, roleid: Optional[int] = None, db: Session = Depends(get_db)):
+def update_user(user_id: int, first_name: Optional[str] = None, last_name: Optional[str] = None, role_id: Optional[int] = None, db: Session = Depends(get_db)):
     user = db.query(User).join(UserRole, User.role_id == UserRole.type_id).filter(User.user_id == user_id).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid User_ID: User not found")
-    if firstname is not None:
-        user.first_name = firstname
-    if lastname is not None:
-        user.last_name = lastname
-    if roleid is not None:
-        user_role = db.query(UserRole).filter(UserRole.type_id == roleid).first()
+    if first_name is not None:
+        user.first_name = first_name
+    if last_name is not None:
+        user.last_name = last_name
+    if role_id is not None:
+        user_role = db.query(UserRole).filter(UserRole.type_id == role_id).first()
         if not user_role:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid roleid: No such role exists")
-        user.role_id = roleid
+        user.role_id = role_id
     db.commit()
     return {
         "message": "User update successful",
         "user": {
-            "User_id": user.user_id,
-            "first_name": user.first_name,
-            "last_name": user.last_name,
-            "role_id": user.role_id
+            "Userid": user.user_id,
+            "firstname": user.first_name,
+            "lastname": user.last_name,
+            "roleid": user.role_id
         }
     }
 
